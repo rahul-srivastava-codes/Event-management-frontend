f95ac4329c159a6f73cbc075aed900e19d73f32c
Rahul Srivastava <117383410+rahul-srivastava-codes@users.noreply.github.com>
2025-12-17 23:36:14 +0530

    Refactor Home component syntax in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="w-full h-[100vh] bg-zinc-200 flex-col items-center justify-center px-[10vw]  py-5">
-      <Home></Home>
+      <Home/>
     </div>
   );
 }
